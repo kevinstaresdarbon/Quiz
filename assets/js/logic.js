f4aed79d3ffbcc4563000adc6d9bf5609c20f5e0
kevinstaresdarbon <red.inshou@gmail.com>
2023-11-06 18:38:19 +0000

    updates logic.js to render and cycle through the questions array with a fade-in effect

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,38 +1,82 @@
+var currentQ = 0;
+var score = 0;
+var time = 120;
 
-function renderQuestion(qObject){
+// get references for the divs where the questions and answers should appear
+var qBox = document.querySelector("#questions");
+var qBoxTitle = document.querySelector("#question-title");
+var aBox = document.querySelector("#choices");
 
-    // get references for the divs where the questions and answers should appear
-    var qBox = document.querySelector("#questions");
-    var qBoxTitle = document.querySelector("#question-title");
-    var aBox = document.querySelector("#choices");
+const sfxCorrect = new Audio("./assets/sfx/correct.wav");
+const sfxIncorrect = new Audio("./assets/sfx/incorrect.wav");
 
-    console.log(aBox);
+function handleAnswerClick(event) {
+
+    var element = event.target;
+
+    var choice = parseInt(element.parentElement.getAttribute("data-index"));
+    if (choice === questions[currentQ].correct) {
+        sfxCorrect.play();
+        score += 1;
+    } else {
+        sfxIncorrect.play();
+        time -= 10;
+    }
+
+    // empty the qBox before re-rendering and increment the currentQ
+    qBoxTitle.textContent = "";
+    aBox.innerHTML = "";
+    currentQ += 1;
+
+    // render the next question
+    renderQuestion(questions[currentQ]);
+}
+
+function renderQuestion(qObject) {
 
     // create the h3 to display the question
     var question = document.createElement("h3");
+
     // set the question from the qObject passed in
     question.textContent = qObject.q;
-
     qBoxTitle.appendChild(question);
 
+    // create an oredered list to hold the answer options
     var answers = document.createElement("ol");
 
-    for (let i = 0; i< qObject.answers.length; i++){
-        
+    // iterate over the answer array for the qObject passed in and create an li and child button for each choice
+
+    for (let i = 0; i < qObject.answers.length; i++) {
+
         var option = document.createElement("li");
         var select = document.createElement("button");
 
         option.textContent = qObject.answers[i];
+        // add a little styling to the li so that the spacing between the button and the question looks nice
         option.setAttribute("style", "display: flex; justify-content: space-between;");
+
+        option.setAttribute("data-index", i)
+
         select.textContent = "Select";
-        
+
+        // add the event listener for each button;
+        select.addEventListener("click", handleAnswerClick);
+
+        // append the button to the li
         option.appendChild(select);
+        // append the li to the ol
         answers.appendChild(option);
+        answers.setAttribute("class", "fade-in-text");
     }
 
+
+
+    // once iteration has finished append the ol to the choices div
     aBox.appendChild(answers);
 
-    qBox.setAttribute("class", "fade-in-text");
+    // change the class here to create a fade-in effect for the ready to display question
+    qBox.setAttribute("class", "show");
+    aBox.setAttribute("class", "show");
 }
 
-renderQuestion(questions[0]);
+renderQuestion(questions[currentQ]);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,21 +2,21 @@ var questions = [
     {
         q: "What kind of bracket is used to denote an Array?",
         answers: ["Square Brackets []", "Parenthesese ()", "Curly Braces {}", "Single Quotes ' ' "],
-        correct: 1
+        correct: 0
     },
     {
         q: "What do we use to contain the condititon for an 'if' statement?",
         answers: ["Square Brackets []", "Parenthesese ()", "Curly Braces {}", "Single Quotes ' ' " ],
-        correct: 2
+        correct: 1
     },
     {
         q: "What do we use to denote an object?",
         answers: ["Square Brackets []", "Parenthesese ()", "Curly Braces {}", "Single Quotes ' ' "],
-        correct: 3
+        correct: 2
     },
     {
         q: "Which of the following is not a type of javascript loop?",
         answers: ["for/in", "function", "do/while", "for/of"],
-        correct: 2
+        correct: 1
     }
 ]
